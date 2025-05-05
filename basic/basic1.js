e927c76c61ddc372eb33177f2d968306ea701e39
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-05 12:27:44 +0530

    datatypes

--- basic/basic1.js
+++ basic/basic1.js
@@ -1,4 +1,41 @@
-console.log("Hello World")
-console.info("information function..")
-console.warn("warning function..")
-console.error("error function..")
+// console.log("Hello World")
+// console.info("information function..")
+// console.warn("warning function..")
+// console.error("error function..")
+
+// DataType : number , string , boolean ,undefined , object
+
+
+a = 10;
+console.log("Value of a = ",a);
+console.log("Type of a = ",typeof(a));
+
+b = 12.5;
+console.log("value of b = ",b);
+console.log("type of b = ",typeof b);
+
+
+c = 'jay'
+console.log("value of c = ",c);
+console.log("type of c = ",typeof c);
+
+isAlive = true;
+console.log("value of isAlive = ",isAlive);
+console.log("type of isAlive = ",typeof isAlive);
+
+var d;
+console.log("value of d = ",d);
+console.log("type of d = ",typeof d);
+
+e = null;
+console.log("value of e = ",e);
+console.log("type of e = ",typeof e);
+
+
+
+
+
+
+
+
+
