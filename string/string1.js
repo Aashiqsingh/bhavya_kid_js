d5292d1567da12c19c5fbc59f26f5e3136528fb3
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-07 12:53:06 +0530

    string

---
+++ string/string1.js
@@ -0,0 +1,57 @@
+let str2 = "bhavya"
+
+// console.log(str);
+// console.log(str.length);
+
+// console.log(str[0]);
+
+// var x = str.charAt(2)
+// console.log(x);
+
+
+// var x = str.charCodeAt(0)
+// console.log(x);
+
+// var x = str.concat(str2)
+// console.log(x);
+
+// var x = str.endsWith("d.")
+// console.log(x);
+
+
+// var x = str.startsWith("A")
+// console.log(x);
+
+// var x = str.includes("soft")
+// console.log(x);
+
+// var x = str.repeat(3)
+// console.log(x);
+
+// var x = str.replaceAll("o","z")
+// console.log(x);
+
+let str = "     Aoyal technosoft pvt ltd.     "
+console.log(str.length);
+
+var x = str.trim()
+console.log(x.length);
+
+// str.trimEnd
+
+
+
+// var x = str.slice(1)
+// var x = str.slice(1,5)
+// var x = str.slice(-4,-1)
+// console.log(x);
+
+
+// var x = str.split("o")
+// var x = str.split(" ")
+// var x = str.split("")
+
+// console.log(x);
+
+// var x = str.toUpperCase()
+// console.log(x);
