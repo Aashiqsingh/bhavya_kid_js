74a9c94e6be081f1c26b2d03efe9ce8a3148c349
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-21 12:33:45 +0530

    map , filter , jsonarray

---
+++ jsonArray/array1.js
@@ -0,0 +1,46 @@
+var users = [
+    {
+        id : 1,
+        name:"pranshul",
+        age:15,
+        email:"pranshul@gmail.com",
+        mobile:'7826397878'
+    },
+    {
+        id:2,
+        name:"rahul",
+        age:29,
+        email:"rahul@gmail.com",
+        mobile:'87263978'
+    },
+    {
+        id:3,
+        name:"rohan",
+        age:27,
+        email:"rohan@gmail.com",
+        mobile:'9826397878'
+    },
+    {
+        id:4,
+        name:"ronali",
+        age:24,
+        email:"sonali@gmail.com",
+        mobile:'8726978'
+    }
+]
+
+
+// users.forEach((user)=>{
+//     console.log(user.name)
+// })
+
+// let x = users.map((user)=>{
+//     return user.name
+// })
+// console.log(x);
+
+
+// let x  = users.filter((user)=>{
+//     return user.age > 25
+// })
+// console.log(x);
